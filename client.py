1626d139bdde71818145a81992d3b42ac08232d1
ankit0918 <136569980+ankit0918@users.noreply.github.com>
2025-03-01 01:18:54 +0545

    Update client.py

--- client.py
+++ client.py
@@ -2,36 +2,31 @@ import socketio
 
 sio = socketio.Client()
 
-username = input("Enter your username: ")
-room = input("Enter room name to join: ")
-
 @sio.event
 def connect():
-    print("Connected to server!")
-    sio.emit('join', {'username': username, 'room': room})
-
-@sio.event
-def status(data):
-    print(data['message'])
-
-@sio.event
-def opponent_move(data):
-    move = data['move']
-    print(f"Opponent's move: {move}")
+    print("Connected to server.")
 
 @sio.event
 def disconnect():
     print("Disconnected from server.")
 
-def send_move(move):
+@sio.on('move')
+def on_move(data):
+    print(f"Received move: {data['move']}")
+    # Update game state in GameController
+
+def connect_to_server(url="http://localhost:5000"):
+    sio.connect(url)
+
+def create_room(room):
+    sio.emit('create_room', {'room': room})
+
+def join_room(room):
+    sio.emit('join_room', {'room': room})
+
+def send_move(room, move):
     sio.emit('move', {'room': room, 'move': move})
 
-sio.connect('http://localhost:5000')
+if __name__ == "__main__":
+    connect_to_server()
 
-while True:
-    move = input("Enter your move (or 'quit' to leave): ")
-    if move.lower() == 'quit':
-        sio.emit('leave', {'username': username, 'room': room})
-        sio.disconnect()
-        break
-    send_move(move)
